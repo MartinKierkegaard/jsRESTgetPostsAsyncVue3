f0b6118842f199083772a1f5c4cdf168ca253fa8
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2021-11-04 17:44:33 +0100

    changed the promise to use arrow function Added a function that get the todos from a single user changed and added stuff in the HTML to support the new function

--- js/index.js
+++ js/index.js
@@ -3,12 +3,13 @@ const baseUri = "http://jsonplaceholder.typicode.com/posts"
 Vue.createApp({
     data() {
         return {
+            nyliste:[],
             posts: [],
             error: null,
             userId: ""
         }
     },
-    async created() {
+     created() {
         // created() is a life cycle method, not an ordinary method
         // created() is called automatically when the page is loaded
         console.log("created method called")
@@ -16,42 +17,65 @@ Vue.createApp({
     },
     methods: {
         cleanList() {
-            this.posts = []
-            this.error = null
-            console.log("count post : " + this.posts.length)
+            this.posts = [];
+            this.error = null;
+            console.log("count post : " + this.posts.length);
         },
-        
+        //Read this for an example: https://vuejs.org/v2/cookbook/using-axios-to-consume-apis.html
          getAllPosts() {
+             //axios call that returns all the elements from the webservice
             axios.get(baseUri)
-            .then(function (response) {
-
+            .then(response => {
              var divtag = document.getElementById("content");
-              //resultElement.innerHTML = generateSuccessHTMLOutput(response);
-              console.log("in helper posts");
-              console.log("response : "+ response.data);
-              console.log("status : " );
-              this.posts = response.data;
+
+             console.log("in function getAllPosts");
+             console.log("status code: "+ response.status );
+
+             //add the returning data from the webservice to the variable posts
+             this.posts = response.data;
               
-              console.log("length post " + this.posts.length)
+             console.log("length of the posts array " + this.posts.length)
 
-              let res = "";
-                for (let i = 0; i < posts.length; i++) {
-                    console.log(this.posts[i].id)
-                    console.log(this.posts[i].title)
+            //   let res = "";
+            //     for (let i = 0; i < posts.length; i++) {
+            //         console.log(this.posts[i].id)
+            //         console.log(this.posts[i].title)
 
-                    res = res + this.posts[i].id + " " + this.posts[i].title + " " + "</br>";
-                }
+            //         res = res + this.posts[i].id + " " + this.posts[i].title + " " + "</br>";
+            //     }
 
-                divtag.innerHTML= res;
+                // divtag.innerHTML= res;
 
             })
-            .catch(function (error) {
+            .catch(error = (ex) => {
               //resultElement.innerHTML = generateErrorHTMLOutput(error);
+              this.posts = []
+               this.error = ex.message
+              console.log("Error:" + this.error);
+            })      
+            
+        },
+        getByUserId(id){
+            //axios call that returns the items from a specified user 
+            axios.get(baseUri + "?userId="  + id)
+            .then(response => {
+            
+            console.log("URI: " + baseUri + "?userId=" +id)
+
+             console.log("in function getByUserId");
+             console.log("status code: "+ response.status );
+
+             //add the returning data from the webservice to the variable posts
+             this.posts = response.data;
+              
+             console.log("length of the posts array " + this.posts.length)
+            })
+            .catch(error = (ex) => {
               this.posts = []
               this.error = ex.message
               console.log("Error:" + this.error);
             })      
-            
+
         },
        
        
